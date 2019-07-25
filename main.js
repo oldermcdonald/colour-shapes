57e5e29439b398b4e2aae76d2f498da4762a355f
Dave <oldmcdonald1@gmail.com>
2019-07-25 14:35:08 +1000

    shapes added and js started

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 // First create div elements
 // then assign random css class shape to div
+
+
+
+const container = document.querySelector('.shape-container');
+
+function randomShape() {
+  let shapes = ['square', 'circle', 'triangle', 'oval', 'rectangle'];
+  return randomElement(shapes);
+}
+
+function randomColour() {
+  let colours = ['blue', 'red', 'green'];
+  return randomElement(colours);
+}
+
+function randomElement(array)  {
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+function addShape() {
+  let shape = document.createElement('div');
+  container.appendChild(shape);
+}
