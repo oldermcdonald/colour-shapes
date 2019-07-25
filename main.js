4e8e0eede6d5f4cefd074a80e778fd2e92e86ab0
Dave <oldmcdonald1@gmail.com>
2019-07-25 15:48:51 +1000

    shapes button reloader working

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// First create div elements
-// then assign random css class shape to div
-
-
+// Code for Selena July 2019
 
 const container = document.querySelector('.shape-container');
+const reloadBtn = document.querySelector('.reload-btn');
 
 function randomShape() {
   let shapes = ['square', 'circle', 'triangle-up','triangle-down', 'oval', 'rectangle'];
@@ -11,14 +9,14 @@ function randomShape() {
 }
 
 function randomColour() {
-  let colours = ['blue', 'red', 'green'];
+  // replace this array with triad colour hex generator algorithm
+  let colours = ['#B3465A', '#FFFA96', '#FF7C93', '50A8CC', '#4F96B3'];
   return randomElement(colours);
 }
 
 function randomElement(array)  {
   return array[Math.floor(Math.random()*array.length)];
 }
-
 
 function applyColour(shape) {
   switch(shape.className) {
@@ -31,15 +29,37 @@ function applyColour(shape) {
     default:
       shape.style.backgroundColor = randomColour();
   }
-
 }
-
 
 function addShape() {
   const shape = document.createElement('div');
   shape.setAttribute("class", randomShape());
-
   applyColour(shape)
-
   container.appendChild(shape);
 }
+
+function renderShapes() {
+  // first remove all children of container
+  let firstShape = container.firstElementChild;
+  while (firstShape) {
+    firstShape.remove();
+    firstShape = container.firstElementChild;
+  }
+
+  // then add children to container
+  addShape()
+  addShape()
+  addShape()
+
+  addShape()
+  addShape()
+  addShape()
+
+  addShape()
+  addShape()
+  addShape()
+}
+
+
+reloadBtn.addEventListener("click", renderShapes);
+renderShapes();
